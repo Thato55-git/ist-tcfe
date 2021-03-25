6d77fde4d9818ee5df0079f3bb8addebe94343c0
Thato <thatomokone10@gmail.com>
2021-03-25 11:23:16 +0000

    Inicial commit

---
+++ t1/mat/Mesh.m
@@ -0,0 +1,18 @@
+pkg load symbolic
+syms R1
+syms R2
+syms R3
+syms R4
+syms R5
+syms R6
+syms R7
+syms Va
+syms Vc
+syms Ib
+syms Id
+Z = vpa(1.0)
+O = vpa(0.0)
+
+C=[Z,O,O,O; O,Z,O,O; O,O,-R7-R4-R6,R4; R3,O,R4,-R1-R3-R4]
+D=[-Ib;Id;Vc;-Va]
+C\D
---
+++ t1/mat/Nodal.m
@@ -0,0 +1,24 @@
+pkg load symbolic
+syms G1
+syms G2
+syms G3
+syms G4
+syms G5
+syms G6
+syms G7
+syms Va
+syms Vc
+syms Ib
+syms Id
+Z = vpa(1.0)
+O = vpa(0.0)
+
+
+A = [-G1-G3-G2, G2, O, G3;-G2, G2, O, O;O, O, G5 -G5;O, O, O, O]
+ 
+ B = [O;
+Ib;
+Id;
+O]
+ 
+ A\B
